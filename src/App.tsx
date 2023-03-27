452e56b7303b8e3618253781115c83141ef8023f
Luiz Antonio Rech <luiz.rech@conffops.com.br>
2023-03-27 19:45:47 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default class App extends Component{
   render() {
     return(
       <View style={estilos.container}>
-        <Text style={estilos.fonte}>
+        <Text style={estilos.fonte}>a
           <Simples/>
         </Text>
       </View>
